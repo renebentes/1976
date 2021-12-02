9fc0b5423feb18c65ddcb4a66d7b78c8714fdd5a
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-12-01 22:06:14 -0300

    feat: define modelos de dados

---
+++ ProductCatalog/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace ProductCatalog.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public IEnumerable<Product> Products { get; set; }
+}
