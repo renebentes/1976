97f0f23f91a02b3bd1852e71b32cd5f6d77304e4
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-10-09 21:24:09 -0300

    styles: Aplica padrão de codificação

--- ProductCatalog/Models/Category.cs
+++ ProductCatalog/Models/Category.cs
@@ -3,6 +3,8 @@ namespace ProductCatalog.Models;
 public class Category
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+
     public IEnumerable<Product> Products { get; set; }
+
+    public string Title { get; set; }
 }
