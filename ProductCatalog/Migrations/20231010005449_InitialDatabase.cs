236c39352845c01488f1b6f3d6cf16005bd2d9ed
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-10-09 22:16:37 -0300

    styles: Aplica padrão de codificação

--- ProductCatalog/Migrations/20231010005449_InitialDatabase.cs
+++ ProductCatalog/Migrations/20231010005449_InitialDatabase.cs
@@ -1,69 +1,67 @@
-﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace ProductCatalog.Migrations
+namespace ProductCatalog.Migrations;
+
+/// <inheritdoc />
+public partial class InitialDatabase : Migration
 {
     /// <inheritdoc />
-    public partial class InitialDatabase : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Categories",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Categories", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Categories",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Categories", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Products",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    CategoryId = table.Column<int>(type: "int", nullable: false),
-                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    LastUpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    Quantity = table.Column<int>(type: "int", nullable: false),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Products", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Products_Categories_CategoryId",
-                        column: x => x.CategoryId,
-                        principalTable: "Categories",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "Products",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                CategoryId = table.Column<int>(type: "int", nullable: false),
+                CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                LastUpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                Quantity = table.Column<int>(type: "int", nullable: false),
+                Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Products", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Products_Categories_CategoryId",
+                    column: x => x.CategoryId,
+                    principalTable: "Categories",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Products_CategoryId",
-                table: "Products",
-                column: "CategoryId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_Products_CategoryId",
+            table: "Products",
+            column: "CategoryId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Products");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Products");
 
-            migrationBuilder.DropTable(
-                name: "Categories");
-        }
+        migrationBuilder.DropTable(
+            name: "Categories");
     }
 }
